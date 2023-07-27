8981665b37a57e91bb8b560bea7a91a85d68df5a
merve <cakirmerve1629@gmail.com>
2023-07-27 16:47:07 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:kurabiye/views/login_view.dart';
+import 'package:kurabiye/views/register_view.dart';
+
+import 'firebase_options.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized;
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const RegisterView(),
+    ),
+  );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("home"),
+      ),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform(),
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              if (user?.emailVerified ?? false) {
+                print('you ara a verified user');
+              } else {
+                print('you need to verify your email first');
+              }
+              return const Text('done');
+            default:
+              return const Text(
+                'loading...',
+                textAlign: TextAlign.center,
+              );
+          }
+        },
+      ),
+    );
+  }
+}
---
+++ lib/views/register_view.dart
@@ -0,0 +1,158 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+import '../firebase_options.dart';
+
+class RegisterView extends StatefulWidget {
+  const RegisterView({super.key});
+
+  @override
+  State<RegisterView> createState() => _RegisterViewState();
+}
+
+class _RegisterViewState extends State<RegisterView> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("register"),
+      ),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform(),
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              return SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Lottie.asset(
+                      'assets/moon.json',
+                      width: 350, // Genişliği 200 yapabilirsiniz.
+                      height: 350,
+                    ),
+                    const Text(
+                      'cookie',
+                      style: TextStyle(
+                        fontFamily: AutofillHints.name,
+                        fontSize: 24,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Center(
+                      child: Container(
+                        alignment: Alignment.bottomCenter,
+                        child: Container(
+                          width: 300, // Sabit genişlik
+                          height: 60,
+                          decoration: BoxDecoration(
+                            image: const DecorationImage(
+                                image: AssetImage('assets/box_back.png'),
+                                fit: BoxFit.cover),
+                            color: Colors.white,
+                            border: Border.all(
+                                color: const Color.fromARGB(255, 244, 233, 136),
+                                width: 2.0),
+                            borderRadius: BorderRadius.circular(50.0),
+                          ),
+                          child: TextField(
+                            controller: _email,
+                            keyboardType: TextInputType.emailAddress,
+                            enableSuggestions: false,
+                            autocorrect: false,
+                            decoration: const InputDecoration(
+                              border: InputBorder.none,
+                              prefixIcon: Icon(Icons.person),
+                              hintText: "enter your email here",
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20), // Boşluk
+                    Container(
+                      alignment: Alignment.bottomCenter,
+                      child: Container(
+                        width: 300, // Sabit genişlik
+                        height: 60,
+                        decoration: BoxDecoration(
+                          image: const DecorationImage(
+                              image: AssetImage('assets/box_back.png'),
+                              fit: BoxFit.cover),
+                          color: Colors.white,
+                          border: Border.all(
+                              color: const Color.fromARGB(255, 244, 233, 136),
+                              width: 2.0),
+                          borderRadius: BorderRadius.circular(50.0),
+                        ),
+                        child: TextField(
+                          controller: _password,
+                          obscureText: true,
+                          enableSuggestions: false,
+                          autocorrect: false,
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            prefixIcon: Icon(Icons.key_rounded),
+                            hintText: "enter your password here",
+                          ),
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () async {
+                        await Firebase.initializeApp(
+                          options: DefaultFirebaseOptions.currentPlatform(),
+                        );
+                        final email = _email.text;
+                        final password = _password.text;
+                        try {
+                          final UserCredential = await FirebaseAuth.instance
+                              .signInWithEmailAndPassword(
+                                  email: email, password: password);
+                          print(UserCredential);
+                        } on FirebaseAuthException catch (e) {
+                          if (e.code == 'weak-password') {
+                            print('Weak-password');
+                          } else if (e.code == 'email-already-in-use') {
+                            print('Email-already-in-use');
+                          }
+                        }
+                      },
+                      child: const Text("register"),
+                    ),
+                    const SizedBox(height: 500),
+                  ],
+                ),
+              );
+            default:
+              return const Text(
+                'loading...',
+                textAlign: TextAlign.center,
+              );
+          }
+        },
+      ),
+    );
+  }
+}

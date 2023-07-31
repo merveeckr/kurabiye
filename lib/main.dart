1c82282f716dd9f0fd8a43f57b4462daddc4d234
merve <cakirmerve1629@gmail.com>
2023-07-31 13:02:36 +0300

    step-2

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
-import 'package:firebase_auth/firebase_auth.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:kurabiye/views/login_view.dart';
-//import 'package:kurabiye/views/register_view.dart';
-
+import 'package:kurabiye/views/register_view.dart';
 import 'firebase_options.dart';
 
 void main() {
@@ -17,6 +16,10 @@ void main() {
         useMaterial3: true,
       ),
       home: const HomePage(),
+      routes: {
+        '/login/': (context) => const LoginView(),
+        '/register/': (context) => const RegisterView(),
+      },
     ),
   );
 }
@@ -26,58 +29,25 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("home"),
+    return FutureBuilder(
+      future: Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform(),
       ),
-      body: FutureBuilder(
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform(),
-        ),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              //final user = FirebaseAuth.instance.currentUser;
-              //if (user?.emailVerified ?? false) {
-              //} else {
-              //  return VerifyEmailView();
-              //}
-              //return const Text('done');
-              return const LoginView();
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            //final user = FirebaseAuth.instance.currentUser;
+            //if (user?.emailVerified ?? false) {
+            //} else {
+            //  return VerifyEmailView();
+            //}
+            //return const Text('done');
+            return const LoginView();
 
-            default:
-              return const Text(
-                'loading...',
-                textAlign: TextAlign.center,
-              );
-          }
-        },
-      ),
-    );
-  }
-}
-
-class VerifyEmailView extends StatefulWidget {
-  const VerifyEmailView({super.key});
-
-  @override
-  State<VerifyEmailView> createState() => _VerifyEmailViewState();
-}
-
-class _VerifyEmailViewState extends State<VerifyEmailView> {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const Text('please verify your email'),
-        TextButton(
-          onPressed: () async {
-            final user = FirebaseAuth.instance.currentUser;
-            await user?.sendEmailVerification();
-          },
-          child: const Text('send email verification'),
-        )
-      ],
+          default:
+            return const CircularProgressIndicator();
+        }
+      },
     );
   }
 }
--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -33,18 +33,52 @@ class _RegisterViewState extends State<RegisterView> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          Lottie.asset(
-            'assets/moon.json',
-            width: 350, // Genişliği 200 yapabilirsiniz.
-            height: 350,
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Lottie.asset(
+              'assets/add.json',
+              width: 350, // Genişliği 200 yapabilirsiniz.
+              height: 350,
+            ),
 
-          const SizedBox(height: 10),
-          Center(
-            child: Container(
+            const SizedBox(height: 10),
+            Center(
+              child: Container(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  width: 300, // Sabit genişlik
+                  height: 60,
+                  decoration: BoxDecoration(
+                    image: const DecorationImage(
+                        image: AssetImage('assets/box_back.png'),
+                        fit: BoxFit.cover),
+                    color: Colors.white,
+                    border: Border.all(
+                        color: const Color.fromARGB(255, 244, 233, 136),
+                        width: 2.0),
+                    borderRadius: BorderRadius.circular(50.0),
+                  ),
+                  child: TextField(
+                    controller: _email,
+                    keyboardType: TextInputType.emailAddress,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      prefixIcon: Icon(Icons.person),
+                      hintText: "enter your email here",
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20), // Boşluk
+            Container(
               alignment: Alignment.bottomCenter,
               child: Container(
                 width: 300, // Sabit genişlik
@@ -60,72 +94,49 @@ class _RegisterViewState extends State<RegisterView> {
                   borderRadius: BorderRadius.circular(50.0),
                 ),
                 child: TextField(
-                  controller: _email,
-                  keyboardType: TextInputType.emailAddress,
+                  controller: _password,
+                  obscureText: true,
                   enableSuggestions: false,
                   autocorrect: false,
                   decoration: const InputDecoration(
                     border: InputBorder.none,
-                    prefixIcon: Icon(Icons.person),
-                    hintText: "enter your email here",
+                    prefixIcon: Icon(Icons.key_rounded),
+                    hintText: "enter your password here",
                   ),
                 ),
               ),
             ),
-          ),
-          const SizedBox(height: 20), // Boşluk
-          Container(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              width: 300, // Sabit genişlik
-              height: 60,
-              decoration: BoxDecoration(
-                image: const DecorationImage(
-                    image: AssetImage('assets/box_back.png'),
-                    fit: BoxFit.cover),
-                color: Colors.white,
-                border: Border.all(
-                    color: const Color.fromARGB(255, 244, 233, 136),
-                    width: 2.0),
-                borderRadius: BorderRadius.circular(50.0),
-              ),
-              child: TextField(
-                controller: _password,
-                obscureText: true,
-                enableSuggestions: false,
-                autocorrect: false,
-                decoration: const InputDecoration(
-                  border: InputBorder.none,
-                  prefixIcon: Icon(Icons.key_rounded),
-                  hintText: "enter your password here",
-                ),
-              ),
-            ),
-          ),
-          TextButton(
-            onPressed: () async {
-              await Firebase.initializeApp(
-                options: DefaultFirebaseOptions.currentPlatform(),
-              );
-              final email = _email.text;
-              final password = _password.text;
-              try {
-                final UserCredential = await FirebaseAuth.instance
-                    .signInWithEmailAndPassword(
-                        email: email, password: password);
-                print(UserCredential);
-              } on FirebaseAuthException catch (e) {
-                if (e.code == 'weak-password') {
-                  print('Weak-password');
-                } else if (e.code == 'email-already-in-use') {
-                  print('Email-already-in-use');
+            TextButton(
+              onPressed: () async {
+                await Firebase.initializeApp(
+                  options: DefaultFirebaseOptions.currentPlatform(),
+                );
+                final email = _email.text;
+                final password = _password.text;
+                try {
+                  final UserCredential = await FirebaseAuth.instance
+                      .signInWithEmailAndPassword(
+                          email: email, password: password);
+                  print(UserCredential);
+                } on FirebaseAuthException catch (e) {
+                  if (e.code == 'weak-password') {
+                    print('Weak-password');
+                  } else if (e.code == 'email-already-in-use') {
+                    print('Email-already-in-use');
+                  }
                 }
-              }
-            },
-            child: const Text("register"),
-          ),
-          const SizedBox(height: 500),
-        ],
+              },
+              child: const Text("register"),
+            ),
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context)
+                      .pushNamedAndRemoveUntil('/login/', (route) => false);
+                },
+                child: const Text('Already registered. Login here!')),
+            const SizedBox(height: 500),
+          ],
+        ),
       ),
     );
   }
